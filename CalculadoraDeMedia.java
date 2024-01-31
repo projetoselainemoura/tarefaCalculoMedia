7c55a5f452d76bd2371e73277af0646075342a04
projetoselainemoura <152440311+projetoselainemoura@users.noreply.github.com>
2024-01-31 15:00:19 -0300

    Add files via upload
    commit

---
+++ CalculadoraDeMedia.java
@@ -0,0 +1,15 @@
+public class CalculadoraDeMedia {
+    public static void main(String[] args) {
+        // Notas fornecidas
+        double nota1 = 8;
+        double nota2 = 9;
+        double nota3 = 7;
+        double nota4 = 9;
+
+        // Calcular a média
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        // Exibir a média
+        System.out.println("A média das notas é: " + media);
+    }
+}
